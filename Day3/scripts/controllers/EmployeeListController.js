1b70bb5e3df8242729a4003542eeec4bcb915f34
AnamTiron <tiron.anam@gmail.com>
2016-06-28 04:05:33 +0300

    Day 3

---
+++ Day3/scripts/controllers/EmployeeListController.js
@@ -0,0 +1,25 @@
+hrApp.controller('EmployeeListController', ['$scope', '$http', '$location','commonResourcesFactoryBackup',
+    function ($scope, $http, $location, commonResourcesFactoryBackup) {
+// TODO #HR2 - inject commonResourcesFactory
+
+        $scope.employees = []; // Employee list
+
+
+        //TODO #HR3 Load employee list from server using commonResourcesFactory
+    $http.get(commonResourcesFactoryBackup.findAllEmployeesUrl)
+        .success(function(data,status,headers,config)
+        {
+            $scope.employees= data;
+        })
+        .error(function(data, status, headers,config){
+            alert("error:"+status);
+        });
+
+
+        $scope.viewEmployee = function (employeeId) {
+            $location.url('/employeeview/' + employeeId);
+        };
+
+
+
+    }]);
